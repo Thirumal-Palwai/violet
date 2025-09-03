45ba18e35b57baf093e015c097ae81f075cc21dd
Thirumal Reddy Palwai <thirumalreddy.palwai@philips.com>
2025-09-03 18:03:02 +0530

    set bash

--- edk2-violet/build.sh
+++ edk2-violet/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # based on the instructions from edk2-platform
-set -e
+set -ex
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p RedmiNote7Pkg/RedmiNote7Pkg.dsc
--- edk2/edksetup.sh
+++ edk2/edksetup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -ex
+
 #
 # Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
 # Copyright (c) 2016, Linaro Ltd. All rights reserved.<BR>
